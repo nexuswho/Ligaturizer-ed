67b9a50fd57a3400b3663e2c59d31f49c3db8e9d
Karthik <nexus@duck.com>
2021-12-08 22:05:33 +0530

    feat. final:test:03

--- scripts/copier.py
+++ scripts/copier.py
@@ -1,8 +1,9 @@
 import os
+os.system("ls - la")
 for root, dirs, files in os.walk(os.path.abspath("../fonts/")):
     for file in files:
         filee = (os.path.join(root, file))
         if "Nerd" in files:
             print(files)
-            commannd = "cp -f " + filee + " ../Ligaturizer/fonts"
+            commannd = "cp -f " + filee + " fonts"
             os.system(commannd)
