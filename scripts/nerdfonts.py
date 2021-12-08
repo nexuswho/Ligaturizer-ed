5c2dde36601c9475050219c2260a8e385074dfb7
Karthik <nexus@duck.com>
2021-12-08 15:03:42 +0530

    test:04

--- scripts/nerdfonts.py
+++ scripts/nerdfonts.py
@@ -5,8 +5,9 @@ list_of_files = []
 for root, dirs, files in os.walk(path):
     for file in files:
         list_of_files.append(os.path.join(root, file))
+
 for name in list_of_files:
     print(name)
-    commandd = "fontforge -script ../nerd-fonts-master/font-patcher  " + name
+    commandd = "fontforge -script ../nerd-fonts-master/font-patcher -c -out ../build/" + name
     print(commandd)
     os.system(commandd)
