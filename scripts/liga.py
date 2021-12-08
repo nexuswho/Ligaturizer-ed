2126ae4e8f3c0eecb71bb0be7c4b5cb6b49ddf70
Karthik <nexus@duck.com>
2021-12-08 22:15:20 +0530

    feat. final:test:04

--- scripts/liga.py
+++ scripts/liga.py
@@ -5,4 +5,4 @@ for root, dirs, files in os.walk(os.path.abspath("../fonts/")):
         files = (os.path.join(root, file))
         print(dirs)
         commannd = "fontforge -lang py -script ligaturize.py" + \
-            files + "--output-dir = ../final - -output-name = "
+            files + "--output-dir = ../final - -output-name = Liga"
