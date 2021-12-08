74f69b30ec41b1fd00eb456ba657df1f7e261ccf
Karthik <nexus@duck.com>
2021-12-08 20:03:09 +0530

    feat. final:test:01

--- scripts/copier.py
+++ scripts/copier.py
@@ -3,5 +3,6 @@ for root, dirs, files in os.walk(os.path.abspath("../fonts/")):
     for file in files:
         files = (os.path.join(root, file))
         if ".ttf" in files or ".otf" in files:
-            commannd = "cp -f " + files + " ../Ligaturizer/fonts"
-            os.system(commannd)
+            print(files)
+            # commannd = "cp -f " + files + " ../Ligaturizer/fonts"
+            # os.system(commannd)
