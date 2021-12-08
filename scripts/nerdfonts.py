8a14f6d4931eafe2a854947575732ff65a1702bf
Karthik <nexus@duck.com>
2021-12-08 12:56:25 +0530

    test:01

---
+++ scripts/nerdfonts.py
@@ -0,0 +1,12 @@
+import os
+path = r'.'
+list_of_files = []
+
+for root, dirs, files in os.walk(path):
+    for file in files:
+        list_of_files.append(os.path.join(root, file))
+for name in list_of_files:
+    print(name)
+    commandd = "fontforge -script ../nerd-fonts/font-patcher  " + name
+    print(commandd)
+    os.system(commandd)
