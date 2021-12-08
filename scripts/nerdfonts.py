0442aabb7c77f1945bf5ffc32a61e49caf372be7
Karthik <nexus@duck.com>
2021-12-08 15:34:56 +0530

    test:05:stupid me:02

--- scripts/nerdfonts.py
+++ scripts/nerdfonts.py
@@ -1,6 +1,7 @@
 import os
+print("Nerdfont patching")
 for i, j, y in os.walk('.'):
     if i != ".":
         print(i)
-        command = "python3 ./nerdfont-patcher.py " + i + " ../build"
+        command = "python3 ../nerdfont-patcher/nerdfont-patcher.py " + i + " ../build"
         os.system(command)
