e1b96a6cbad84b90f80394e77673d489b08f896c
Karthik <nexus@duck.com>
2021-12-09 12:11:11 +0530

    why:3

--- scripts/liga.py
+++ scripts/liga.py
@@ -2,13 +2,11 @@ import os
 os.system("ls -la to-be-patched")
 for root, dirs, files in os.walk(os.path.abspath("./to-be-patched/")):
     for file in files:
-        print(file)
         filee = (os.path.join(root, file))
-        print(filee)
         filles = f'"{filee}"'
         print(filles)
         file = f'"{file}"'
         print(file)
         commannd = "fontforge -lang py -script ligaturize.py " + \
             filles + " --output-dir = ../final - -output-name=" + file
-        # os.system(commannd)
+        os.system(commannd)
